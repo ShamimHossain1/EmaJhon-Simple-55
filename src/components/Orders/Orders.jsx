0c3dfecddd7891c37771797eae3b8eb5a69c8d9b
ShamimHossain1 <beastb697@gmail.com>
2023-04-09 01:38:23 +0600

    remove from order

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
+import ReviewItem from '../ReviewItems/ReviewItem';
+import { removeFromDb } from '../../utilities/fakedb';
 
 const Orders = () => {
     const products = useLoaderData();
-    console.log(products);
+    const [cart, setCart] = useState(products);
+
+    const handleRemoveCart = (id) => {
+            const remaining= cart.filter(product => product.id !==id);
+            setCart(remaining);
+            removeFromDb(id);
+
+
+    }
+    // console.log(products);
     return (
-        <div className='flex mt-10 px-10'>
-            <div className=' grid grid-cols-3 gap-8'>
-                    <h1>Hello world </h1>
+        <div className='flex mt-10 px-52'>
+            <div className='w-6/12 mx-auto '>
+                {
+                    cart.map(product => <ReviewItem
+                        key={product.id}
+                        product={product}
+                        handleRemoveCart={handleRemoveCart}
+
+                    ></ReviewItem>)
+                }
             </div>
-            <div className="w-2/4 ml-8 bg-yellow-200 rounded-lg">
-                <Cart cart={products} ></Cart>
+            <div className=" w-4/12  bg-yellow-200 rounded-lg ">
+                <Cart cart={cart} ></Cart>
             </div>
         </div>
     );
